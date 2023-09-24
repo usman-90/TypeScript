948b7a1fae1d1959f450e7e2be65a853e91eeaf4
Usman Siddiqui <siddiquiusman328@gmail.com>
2023-09-24 17:16:46 +0500

    ..

--- src/DSA/trees.ts
+++ src/DSA/trees.ts
@@ -32,18 +32,25 @@ const inOrderTraverse = <T>(node: treeNode<T>): void => {
 	if (!node?.data) {
 		return
 	}
-	console.log(node?.data);
 	inOrderTraverse(node?.left);
+	console.log(node?.data);
 	inOrderTraverse(node?.right);
 
 };
-let i : number = 0
+let i: number = 0
 const seti = () => {
 	i = 0
 }
+
+const findMin = <T>(node: treeNode<T>): treeNode<T> => {
+	if (!node?.left) {
+		return node;
+	}
+
+	return findMin(node.left)
+}
 const deleteNode = <T>(node: treeNode<T>, x: T): void => {
 	if (!node.left && !node.right && node.data === x) {
-		console.log("Executed")
 		node.data = null;
 		return
 	}
@@ -63,17 +70,31 @@ const deleteNode = <T>(node: treeNode<T>, x: T): void => {
 		return
 
 	}
-	if (node?.data > x){
-		deleteNode(node.left,x)
+	else if (node?.left && node?.right && node?.data === x) {
+		let temp: treeNode<T> = findMin(node?.right)
+		node.data = temp.data;
+		if (temp?.right) {
+			temp.data = temp.right.data
+			temp.left = temp?.right?.left
+			temp.right = temp?.right?.left
+		}
+		return
+
 	}
-	else if (node?.data < x){
-		deleteNode(node.right,x)
+	if (node?.data > x) {
+		deleteNode(node.left, x)
+	}
+	else if (node?.data < x) {
+		deleteNode(node.right, x)
 	}
 }
 
-
-
-
+//			10
+//		5 		19
+//	3 		 15		25
+//2 		4 		22 		27
+// 			20
+//   				21
 let node: treeNode<number> = {};
 insert(node, 10);
 insert(node, 5);
@@ -81,24 +102,14 @@ insert(node, 3);
 insert(node, 4);
 insert(node, 2);
 insert(node, 19);
+insert(node, 15);
 insert(node, 25);
 insert(node, 22);
 insert(node, 27);
+insert(node, 20);
+insert(node, 21)
 inOrderTraverse(node);
 console.log("After deletion!")
-deleteNode(node,19)
+deleteNode(node, 19)
 inOrderTraverse(node);
 
-// class Tree<T> {
-//   node: treeNode<T>;
-//   treeLength: number;
-//   treeSize: number;
-//   constructor(size: number) {
-//     this.treeSize = size;
-//     this.node = {
-//       data: undefined,
-//       left: undefined,
-//       right: undefined,
-//     };
-//   }
-// }
